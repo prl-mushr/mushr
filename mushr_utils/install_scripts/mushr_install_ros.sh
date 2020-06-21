3f28cac75227da8530b6e460d07a0cbf51b26553
planc509 <planc509@cs.washington.edu>
2020-06-21 15:38:42 -0700

    Add drafts of some scripts useful for installing mushr on jetson nano

---
+++ mushr_utils/install_scripts/mushr_install_ros.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Setup software sources from packages.ros.org
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+# Setup keys
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+# Update package index
+sudo apt update
+
+# Install ROS libraries
+sudo apt install ros-melodic-desktop
+
+# Auto source ROS on terminal startup
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+
+# Source it for this terminal
+source ~/.bashrc
+
+# Initialize rosdep
+sudo rosdep init
+
+# Update rosdep
+rosdep update
---
+++ mushr_utils/install_scripts/mushr_install_stack.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Install git
+sudo apt install git-all
+
+# Install tkinter
+sudo apt install python-tk
+
+# Install vcstool
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xAB17C654
+sudo apt-get update
+sudo apt-get install python3-vcstool
+
+# Install pip
+sudo apt install python-pip
+
+# Install extra ROS packages
+sudo apt install -y ros-melodic-ackermann-msgs ros-melodic-map-server ros-melodic-serial ros-melodic-urg-node ros-melodic-robot-state-publisher ros-melodic-xacro
+
+# Create workspace
+cd ~
+mkdir -p catkin_ws/src
+cd catkin_ws/src
+catkin_init_workspace
+
+# Get repo info
+sudo apt install wget
+wget https://mushr.io/tutorials/quickstart/repos.yaml
+
+# Clone the repos
+vcs import < repos.yaml
+
+# Install dependencies
+rosdep install --from-paths src --ignore-src -r -y
+
+# Install Cython
+sudo pip install Cython
+
+# Install rangelibc
+cd ~/catkin_ws/src/range_libc/pywrapper
+sudo python setup.py install
+cd ~/catkin_ws/src
+rm -rf range_libc
+
+# Compile
+cd ~/catkin_ws/src
+catkin_make
+
+# Create default RVIZ setup
+mkdir ~/.rviz
+cp ~/catkin_ws/src/mushr/mushr_utils/rviz/default.rviz ~/.rviz/
