894468dbb9b10b60869ebcc2456c1d3168722a4f
ros-melodic-vmware <markus.mbox@gmail.com>
2021-06-14 15:51:39 -0700

    updated repos YAML link

--- mushr_utils/install_scripts/mushr_install_stack.sh
+++ mushr_utils/install_scripts/mushr_install_stack.sh
@@ -37,7 +37,7 @@ echo "export ROS_IP=10.42.0.1" >> ~/.bashrc
 # Get repo info
 sudo apt install wget
 cd ~/catkin_ws/src
-wget https://mushr.io/tutorials/quickstart/repos.yaml
+wget https://raw.githubusercontent.com/prl-mushr/mushr/master/repos.yaml
 
 # Clone the repos
 vcs import < repos.yaml
