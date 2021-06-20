5a0a83f3fa1479997a2c78b421fa9ad7e68e9973
planc509 <planc509@cs.washington.edu>
2021-06-20 14:01:45 -0700

    Update hw drivers script for realsense install fix

--- mushr_utils/install_scripts/mushr_install_hw_drivers.sh
+++ mushr_utils/install_scripts/mushr_install_hw_drivers.sh
@@ -3,11 +3,12 @@
 # Need to reboot after running this script
 
 # Install librealsense. Following commands copied from
-# https://github.com/JetsonHacksNano/installLibrealsense/blob/master/installLibrealsense.sh
-sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key
-sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
+# https://github.com/IntelRealSense/librealsense/blob/master/doc/distribution_linux.md
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
+sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F42ED6FBAB17C654
 sudo apt-get install apt-utils -y
-sudo apt-get install librealsense2-utils librealsense2-dev -y
+sudo apt-get install librealsense2-utils librealsense2-dev librealsense2-dkms -y
 
 # Install YDLidar drivers
 cd ~/catkin_ws/src/mushr/mushr_hardware
