bc175559fe57f1837dec1570d024c850c90d0a3e
Michael Jae-Yoon Chung <mikejaeyoon@gmail.com>
2023-06-09 08:24:05 -0700

    Remove custom caddy install script

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-# Install caddy
-export DEBIAN_FRONTEND=noninteractive
-sudo apt update && sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-sudo apt update && sudo apt install -y caddy
-
 # Install foxglove studio
 cleanup() {
     EXIT_CODE=$?
