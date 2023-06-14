b142ece277d022b8f07121446e71345aed0ec046
Michael Jae-Yoon Chung <mikejaeyoon@gmail.com>
2023-06-13 21:00:33 -0700

    Handle comment

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-cd /home/vscode/catkin_ws/src;
-./mushr/mushr_utils/install/mushr_install.bash --trivial-only;
+# This script is meant to be executed within the "postCreateCommand" field in
+# .devcontainer/devcontainer.json, i.e., it expects the devcontainer config to install
+# necessary folders with source code beforehand.
+
+set -eo pipefail
+set -x
+
+cd "${HOME}"/catkin_ws/src
+./mushr/mushr_utils/install/mushr_install.bash --trivial-only
 mushr_noetic run 'cd catkin_ws && catkin build'
